98a4737d35be4593034137d4fb449f3c1bdb337b
Sowmiya Nagarajan <sowmiya_n@intuit.com>
2020-03-29 15:27:00 +0530

    New release

--- stackbox.rb
+++ stackbox.rb
@@ -1,17 +1,18 @@
 class Stackbox < Formula
   desc "Create app stacks loaded with all your favourite clients, services and infra along with code boilerplates in under 5 mins."
   homepage ""
-  url "https://github.com/Stack-Box/StackBox/archive/v1.0.0.tar.gz"
-  sha256 "d0806cd02f5b51eb7f2f0ec9fb438b5de768f9782db88cdeb0d772ccf0365239"
+  url "https://github.com/Stack-Box/StackBox/archive/v1.2.0.tar.gz"
+  sha256 "ea9bcc6905169868ac4f5ee54b7ace6ad8ed8416d482accbcf3fb7211027436b"
 
   depends_on "docker"
   depends_on "docker-compose"
   depends_on "python"
+  depends_on "shc"
 
   def install
-    system "mkdir -p /usr/local/Cellar/stackbox/1.0.0"
-    system "cp -a . /usr/local/Cellar/stackbox/1.0.0/"
-    system "mkdir /usr/local/Cellar/stackbox/1.0.0/bin"
-    system "mv /usr/local/Cellar/stackbox/1.0.0/stack_box.sh /usr/local/Cellar/stackbox/1.0.0/bin/stackbox.sh"
+    system "mkdir -p /usr/local/Cellar/stackbox/1.2.0/bin"
+    system "cp -a . /usr/local/Cellar/stackbox/1.2.0/"
+    system "shc -f ./brew/stackbox-brew.sh -o stackbox"
+    system "cp ./stackbox /usr/local/Cellar/stackbox/1.2.0/bin/"
   end
 end
